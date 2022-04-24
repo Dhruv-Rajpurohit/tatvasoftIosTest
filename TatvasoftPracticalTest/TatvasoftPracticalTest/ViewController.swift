5f63663a42b42ea2850578f0bce649280a70b7fd
Dhruv Rajpurohit <dhruv.n.rajpurohit@gmail.com>
2022-04-24 09:58:15 +0530

    Basic Project Setup

---
+++ TatvasoftPracticalTest/TatvasoftPracticalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TatvasoftPracticalTest
+//
+//  Created by Dhruv Rajpurohit on 24/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
